f73daec4da42105e042e79fdeeafceb8b7be5949
cfsnook <cfs@soton.ac.uk>
2022-07-25 13:06:09 +0100

    fix copyrights

--- ac.soton.eventb.emf.core.extension/src/ac/soton/eventb/emf/core/extension/coreextension/impl/ValueImpl.java
+++ ac.soton.eventb.emf.core.extension/src/ac/soton/eventb/emf/core/extension/coreextension/impl/ValueImpl.java
@@ -1,13 +1,16 @@
-/**
- * Copyright (c) 2012-2021 - University of Southampton.
- * 
+/*******************************************************************************
+ * Copyright (c) 2011, 2021 University of Southampton.
+ *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * https://www.eclipse.org/legal/epl-2.0/
  *
- * $Id$
- */
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    University of Southampton - initial API and implementation
+ *******************************************************************************/
 package ac.soton.eventb.emf.core.extension.coreextension.impl;
 
 import ac.soton.eventb.emf.core.extension.coreextension.CoreextensionPackage;
